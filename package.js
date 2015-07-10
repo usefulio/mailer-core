f4cf0bb22e33e42f4d7c9426fd92f8fdf176d3b4
Joshua Ohlman <codewriterohs@gmail.com>
2015-07-10 07:35:27 -0500

    Add underscore as a dependency

--- package.js
+++ package.js
@@ -12,8 +12,9 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.2');
-  api.addFiles('mailer-core.js');
+  api.use('underscore');
 
+  api.addFiles('mailer-core.js');
   api.export('Mailer');
 });
 
